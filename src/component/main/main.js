018d6357dd8ab881520260625c7b1e52292402ac
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-01-07 19:47:29 +0530

    Add files via upload

---
+++ src/component/main/main.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="arc1">
+          <div className="card">
+            <h2>Bot Features</h2>
+            <p>
+              1. Advance Moderation <br />
+              2. Music System <br />
+              3. Reaction Roles <br />
+              4. Ticket System <br />
+            </p>
+          </div>
+        </div>
+        <div className="arc2">
+        <div className="card">
+            <h2>Bot Stats</h2>
+            <p>
+              1. 500 Commands <br />
+              2. 800 Sub Commads <br />
+              3. 29999  Servers <br />
+              4. 5555555 Users <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
