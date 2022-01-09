060c27f3c6f5eaff114514292b77b8b7540905d9
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-01-09 19:20:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./component/navbar/nav";
+import Main from "./component/main/main";
+import Footer from "./component/Footer/footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <>
+     <div className="footer">
+         <p>Coded By Tech Boy Gaming</p>
+     </div>
+    </>
+  );
+};
+
+export default Footer;
